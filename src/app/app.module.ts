007daee89438ff073a64d832262466efd5c059f9
litusmov <litus@movcall.com>
2019-10-26 02:32:46 +0200

    config auth login & methods & event submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,7 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [AngularFireAuth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private fAuth: AngularFireAuth) { }
+
+  registerUser() {}
+
+  loginEmailUser(email: string, password: string){
+      return new Promise((resolve, reject)=>{
+        this.fAuth.auth.signInWithEmailAndPassword(email, password)
+        .then(userData => resolve(userData),
+        err => reject(err));
+      });
+  }
+
+  logoutUser(){
+    return this.fAuth.auth.signOut();
+  }
+
+  isAuth(){
+    this.fAuth.authState.pipe(map(auth => auth));
+  }
 }
